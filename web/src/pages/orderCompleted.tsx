8c57fe3653bd9077cc90481ecbbf3e537568bd6e
João Vitor Lima <jvolima2004@gmail.com>
2022-07-19 09:44:22 -0300

    finalizando tela de confirmação do pedido

--- web/src/pages/orderCompleted.tsx
+++ web/src/pages/orderCompleted.tsx
@@ -5,17 +5,22 @@ import { useCart } from "../hooks/cart";
 import { 
   Container, 
   Content, 
+  IconCircle, 
+  Info, 
+  InfoDescription, 
+  Infos, 
+  InfoTitle, 
   OrderDetails, 
   Subtitle, 
+  Texts, 
   Title 
 } from "./styles/orderCompleted";
 import deliveryImage from "../../public/images/delivery.svg";
 import Image from "next/image";
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
 
 export default function OrderCompleted() {
   const { order } = useCart();
-
-  console.log(order)
 
   return (
     <>
@@ -29,7 +34,42 @@ export default function OrderCompleted() {
 
         <Content>
           <OrderDetails>
+            <Infos>
+              <Info>
+                <IconCircle color="#8047F8">
+                  <MapPin size={16} weight="fill" color="#FAFAFA" />
+                </IconCircle>
+                <InfoTitle>
+                  Entrega em <b>{order.address}, {order.number}</b> <br/> {order.district} - {order.city}, {order.uf}
+                </InfoTitle>
+              </Info>
 
+              <Info>
+                <IconCircle color="#DBAC2C">
+                  <Timer size={16} weight="fill" color="#FAFAFA" />
+                </IconCircle>
+                <Texts>
+                  <InfoTitle>Previsão de entrega</InfoTitle>
+                  <InfoDescription>20 min - 30 min</InfoDescription>
+                </Texts>
+              </Info>
+
+              <Info>
+                <IconCircle color="#C47F17">
+                  <CurrencyDollar size={16} weight="fill" color="#FAFAFA" />
+                </IconCircle>
+                <Texts>
+                  <InfoTitle>Pagamento na entrega</InfoTitle>
+                  <InfoDescription>
+                    { 
+                      order.paymentMethod === "credit-card" ? "Cartão de Crédito" 
+                      : order.paymentMethod === "debit-card" ? "Cartão de Débito" 
+                      : "Dinheiro" 
+                    }
+                  </InfoDescription>
+                </Texts>
+              </Info>
+            </Infos>
           </OrderDetails>
           <Image src={deliveryImage} alt="Deliveryman image" />
         </Content>
--- web/src/pages/styles/orderCompleted.ts
+++ web/src/pages/styles/orderCompleted.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface IconCircleProps {
+  color: string;
+}
+
 export const Container = styled.div`
   max-width: 1120px;
   width: 100%;
@@ -31,4 +35,67 @@ export const Content = styled.div`
 `;
 
 export const OrderDetails = styled.div`
+  max-width: 526px;
+  width: 100%;
+  padding: 40px;
+  background-color: var(--background);
+  background-clip: padding-box;
+
+  border: 1px solid transparent;
+  border-radius: 6px 36px;
+  box-sizing: border-box;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0; right: 0; bottom: 0; left: 0;
+    z-index: -1;
+    margin: -1px; 
+    border-radius: inherit; 
+    background: linear-gradient(to right, var(--yellow), var(--purple));
+  }
+`;
+
+export const Infos = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px 0px;
+`;
+
+export const IconCircle = styled.div<IconCircleProps>`
+  width: 32px;
+  height: 32px;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 32px;
+  background-color: ${({ color }) => color};
+  margin-right: 12px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Texts = styled.div`
+`;
+
+export const InfoTitle = styled.span`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+  color: var(--text);
+  line-height: 130%;
+`;
+
+export const InfoDescription = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  font-size: 1rem;
+  color: var(--text);
+  line-height: 130%;
 `;
